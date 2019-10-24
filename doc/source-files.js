f6f94bb5dcceba62648ead491b886b1b04b0dd79
Deployment Bot (from Azure Pipelines) <deploy@tokio-rs.com>
2019-10-24 09:11:10 +0000

    Deploy Mio API documentation

---
+++ doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["mio"] = {"name":"","dirs":[{"name":"event","files":["event.rs","events.rs","mod.rs","source.rs"]},{"name":"net","dirs":[{"name":"uds","files":["datagram.rs","listener.rs","mod.rs","stream.rs"]}],"files":["mod.rs","tcp_listener.rs","tcp_stream.rs","udp.rs"]},{"name":"sys","dirs":[{"name":"unix","dirs":[{"name":"uds","files":["datagram.rs","listener.rs","mod.rs","stream.rs"]}],"files":["epoll.rs","mod.rs","net.rs","sourcefd.rs","tcp_listener.rs","tcp_stream.rs","udp.rs","waker.rs"]}],"files":["mod.rs"]}],"files":["interests.rs","lib.rs","poll.rs","token.rs","waker.rs"]};
+createSourceSidebar();
